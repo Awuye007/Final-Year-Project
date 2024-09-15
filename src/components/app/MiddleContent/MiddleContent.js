cc46dedc5a491df3c85544d7bcfe51fcca8e02e4
Jeffrey Nunya Awuye <awuyejeffrey@gmail.com>
2024-09-15 22:22:34 +0000

    Making some minor changes

--- src/components/app/MiddleContent/MiddleContent.js
+++ src/components/app/MiddleContent/MiddleContent.js
@@ -1,22 +1,46 @@
 import React from "react";
 import Post from "../Post/Post";
 import "./MiddleContent.css";
+import { useRef } from "react";
 
 export const MiddleContent = () => {
-  return (
-    <div className="flex-1 flex-grow text-white overflow-y-auto pb-20 scroll-bg-darkgrey pe-2">
-      <div className="bg-grey flex justify-around gap-5 p-4 rounded-2xl mb-5">
-        <img
-          src="https://picsum.photos/200/300"
-          alt="profile"
-          className="w-12 h-12 rounded-full"
-        />
-        <div className="flex flex-col flex-1 gap-5">
-          <input
-            className="bg-darkgrey text-white placeholder:text-slate-400 w-full outline-none text-sm rounded-xl p-2 h-12 px-4"
-            placeholder="What's happening?"
-          />
-          {/* <div className="flex gap-3 items-center">
+	const [input, setInput] = React.useState("");
+	const submitButtonRef = useRef(null);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Daabase stuff
+
+		setInput("");
+	};
+
+	return (
+		<div className="flex-1 flex-grow text-white overflow-y-auto pb-20 scroll-bg-darkgrey pe-2">
+			<div className="bg-grey flex justify-around gap-5 p-7 rounded-2xl mb-5  message-sender">
+				<img
+					src="https://picsum.photos/200/300"
+					alt="profile"
+					className="w-12 h-12 rounded-full"
+				/>
+				<div className="flex  flex-1 gap-5 ">
+					<input
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								// Trigger form submit when Enter key is pressed
+								submitButtonRef.current.click();
+							}
+						}}
+						className="bg-darkgrey text-white placeholder:text-slate-400 w-full outline-none text-sm rounded-xl p-2 h-12 px-4"
+						placeholder="What's happening?"
+					/>
+
+					<button onClick={handleSubmit} ref={submitButtonRef} type="submit">
+						Hidden submit
+					</button>
+					{/* <div className="flex gap-3 items-center">
             <button className="border-slate-100 border p-2 px-4 rounded-full flex items-center gap-3 hover:bg-slate-500 transition">
               <i className="fa-solid fa-image text-green-400 text-xl"></i>
               Picture
@@ -30,11 +54,11 @@ export const MiddleContent = () => {
               Schedule
             </button>
           </div> */}
-        </div>
-      </div>
-      {Array.from({ length: 10 }).map((_, i) => (
-        <Post key={i} />
-      ))}
-    </div>
-  );
+				</div>
+			</div>
+			{Array.from({ length: 10 }).map((_, i) => (
+				<Post key={i} />
+			))}
+		</div>
+	);
 };
--- src/components/ui/Navbar/Navbar.js
+++ src/components/ui/Navbar/Navbar.js
@@ -3,97 +3,97 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export const Navbar = ({ open, setOpen }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  return (
-    <div className="flex py-4">
-      <div className="flex-1 flex items-center gap-4">
-        <h1 className="text-white">LIV</h1>
-        <div className="flex bg-grey w-60 items-center gap-3 p-2 rounded-xl">
-          <i className="fa-solid fa-hashtag text-muted"></i>
-          <input
-            className="bg-transparent text-white w-full outline-none text-sm"
-            placeholder="Explore"
-          />
-        </div>
-      </div>
-      <div className="flex-1 justify-end flex items-center gap-4">
-        <div className="flex gap-10">
-          <button className="hover:opacity-80 transition flex items-center gap-3">
-            <i className="fa-solid fa-envelope text-lightgrey text-xl"></i>
-          </button>
-          <button className="hover:opacity-80 transition flex items-center gap-3">
-            <i className="fa-solid fa-bell text-lightgrey text-xl"></i>
-          </button>
-        </div>
-        <div className="flex gap-4 border-l border-opacity-50 pl-4 border-l-slate-300">
-          <Menu as="div" className="relative inline-block text-left">
-            <div>
-              <MenuButton className="flex items-center gap-3 bg-grey p-2 py-1 rounded-full hover:opacity-80 transition-all">
-                <img
-                  src="https://img.icons8.com/ios-filled/50/FFFFFF/user-male-circle.png"
-                  alt="Profile"
-                  className="rounded-full"
-                  width={35}
-                />
-                <span className="text-muted text-base">Jason Addy</span>
-                <i className="fa-solid fa-sort-down text-lightgrey relative -top-1"></i>
-              </MenuButton>
-            </div>
+	return (
+		<div className="flex py-4">
+			<div className="flex-1 flex items-center gap-4">
+				<h1 className="text-white">LIV</h1>
+				<div className="flex bg-grey w-60 items-center gap-3 p-2 rounded-xl">
+					<i className="fa-solid fa-hashtag text-muted"></i>
+					<input
+						className="bg-transparent text-white w-full outline-none text-sm"
+						placeholder="Explore"
+					/>
+				</div>
+			</div>
+			<div className="flex-1 justify-end flex items-center gap-4">
+				<div className="flex gap-10">
+					<button className="hover:opacity-80 transition flex items-center gap-3">
+						<i className="fa-solid fa-envelope text-lightgrey text-xl"></i>
+					</button>
+					<button className="hover:opacity-80 transition flex items-center gap-3">
+						<i className="fa-solid fa-bell text-lightgrey text-xl"></i>
+					</button>
+				</div>
+				<div className="flex gap-4 border-l border-opacity-50 pl-4 border-l-slate-300">
+					<Menu as="div" className="relative inline-block text-left">
+						<div>
+							<MenuButton className="flex items-center gap-3 bg-grey p-2 py-1 rounded-full hover:opacity-80 transition-all">
+								<img
+									src="https://img.icons8.com/ios-filled/50/FFFFFF/user-male-circle.png"
+									alt="Profile"
+									className="rounded-full"
+									width={35}
+								/>
+								<span className="text-muted text-base">Mabel Awuye</span>
+								<i className="fa-solid fa-sort-down text-lightgrey relative -top-1"></i>
+							</MenuButton>
+						</div>
 
-            <MenuItems
-              transition
-              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-grey shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-            >
-              <div className="py-1">
-                <MenuItem>
-                  <Link
-                    to={"/profile"}
-                    className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-500"
-                  >
-                    Profile
-                  </Link>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="/"
-                    className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-500"
-                  >
-                    Support
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="/"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setOpen(true);
-                    }}
-                    className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-500"
-                  >
-                    Change Password
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <button
-                    type="button"
-                    onClick={() => navigate("/auth/login")}
-                    className="block w-full px-4 py-2 text-left text-sm text-white data-[focus]:bg-gray-500"
-                  >
-                    Sign out
-                  </button>
-                </MenuItem>
-              </div>
-            </MenuItems>
-          </Menu>
-          <button className="hover:opacity-80 transition">
-            <img
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAbElEQVR4nO3VSwqAMAyE4d6gHlKLh1av8kshPtDWhWahOB9kNe1mICQE+SWgAZJN9M6L7NPEZtx/fppXAT1nySuvsnqOWq+8yqrK9SyGQpW380v5IdDZRO9cVtpjtMdoj78O3WN0j9E9lveaAewKCHZ5+IXWAAAAAElFTkSuQmCC"
-              alt="Menu"
-            />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+						<MenuItems
+							transition
+							className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-grey shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+						>
+							<div className="py-1">
+								<MenuItem>
+									<Link
+										to={"/profile"}
+										className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-500"
+									>
+										Profile
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<a
+										href="/"
+										className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-500"
+									>
+										Support
+									</a>
+								</MenuItem>
+								<MenuItem>
+									<a
+										href="/"
+										onClick={(e) => {
+											e.preventDefault();
+											setOpen(true);
+										}}
+										className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-500"
+									>
+										Change Password
+									</a>
+								</MenuItem>
+								<MenuItem>
+									<button
+										type="button"
+										onClick={() => navigate("/auth/login")}
+										className="block w-full px-4 py-2 text-left text-sm text-white data-[focus]:bg-gray-500"
+									>
+										Sign out
+									</button>
+								</MenuItem>
+							</div>
+						</MenuItems>
+					</Menu>
+					<button className="hover:opacity-80 transition">
+						<img
+							src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAbElEQVR4nO3VSwqAMAyE4d6gHlKLh1av8kshPtDWhWahOB9kNe1mICQE+SWgAZJN9M6L7NPEZtx/fppXAT1nySuvsnqOWq+8yqrK9SyGQpW380v5IdDZRO9cVtpjtMdoj78O3WN0j9E9lveaAewKCHZ5+IXWAAAAAElFTkSuQmCC"
+							alt="Menu"
+						/>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
