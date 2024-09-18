498b7b2bbc39cffee6315408942debeb4e3234c5
Jeffrey Nunya Awuye <awuyejeffrey@gmail.com>
2024-09-18 08:00:23 +0000

    minor changes

--- src/components/app/MiddleContent/MiddleContent.js
+++ src/components/app/MiddleContent/MiddleContent.js
@@ -3,38 +3,38 @@ import Post from "../Post/Post";
 import "./MiddleContent.css";
 
 export const MiddleContent = () => {
-  const [input, setInput] = React.useState("");
-  const submitButtonRef = useRef(null);
+	const [input, setInput] = React.useState("");
+	const submitButtonRef = useRef(null);
 
-  const handleSubmit = () => {
-    // Daabase stuff
+	const handleSubmit = () => {
+		// Database stuff
 
-    setInput("");
-  };
+		setInput("");
+	};
 
-  return (
-    <div className="flex-1 flex-grow text-white overflow-y-auto pb-20 scroll-bg-darkgrey pe-2">
-      <div className="bg-grey flex justify-around gap-5 p-7 rounded-2xl mb-5  message-sender">
-        <img
-          src="https://picsum.photos/200/300"
-          alt="profile"
-          className="w-12 h-12 rounded-full"
-        />
-        <div className="flex flex-1 gap-5 ">
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                // Trigger form submit when Enter key is pressed
-                handleSubmit();
-              }
-            }}
-            className="bg-darkgrey text-white placeholder:text-slate-400 w-full outline-none text-sm rounded-xl p-2 h-12 px-4"
-            placeholder="What's happening?"
-          />
+	return (
+		<div className="flex-1 flex-grow text-white overflow-y-auto pb-20 scroll-bg-darkgrey pe-2">
+			<div className="bg-grey flex justify-around gap-5 p-7 rounded-2xl mb-5  message-sender">
+				<img
+					src="https://picsum.photos/200/300"
+					alt="profile"
+					className="w-12 h-12 rounded-full"
+				/>
+				<div className="flex flex-1 gap-5 ">
+					<input
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								// Trigger form submit when Enter key is pressed
+								handleSubmit();
+							}
+						}}
+						className="bg-darkgrey text-white placeholder:text-slate-400 w-full outline-none text-sm rounded-xl p-2 h-12 px-4"
+						placeholder="What's happening?"
+					/>
 
-          {/* <div className="flex gap-3 items-center">
+					{/* <div className="flex gap-3 items-center">
             <button className="border-slate-100 border p-2 px-4 rounded-full flex items-center gap-3 hover:bg-slate-500 transition">
               <i className="fa-solid fa-image text-green-400 text-xl"></i>
               Picture
@@ -48,11 +48,11 @@ export const MiddleContent = () => {
               Schedule
             </button>
           </div> */}
-        </div>
-      </div>
-      {Array.from({ length: 10 }).map((_, i) => (
-        <Post key={i} />
-      ))}
-    </div>
-  );
+				</div>
+			</div>
+			{Array.from({ length: 10 }).map((_, i) => (
+				<Post key={i} />
+			))}
+		</div>
+	);
 };
