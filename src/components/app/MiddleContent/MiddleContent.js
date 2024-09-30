9e7b6aff9ae44f2b8cb5b13d916bdf9426b90f6f
Jeffrey Nunya Awuye <awuyejeffrey@gmail.com>
2024-09-30 03:30:31 +0000

    making minor changes

--- src/components/app/MiddleContent/MiddleContent.js
+++ src/components/app/MiddleContent/MiddleContent.js
@@ -1,56 +1,56 @@
 import React from "react";
-import { postComment } from "../../../shared/services/post.service";
+import { createPost } from "../../../shared/services/post.service";
 import Post from "../Post/Post";
 import "./MiddleContent.css";
 
 export const MiddleContent = () => {
-  const [search, setSearch] = React.useState("");
+	const [search, setSearch] = React.useState("");
 
-  const handleSubmit = async () => {
-    try {
-      const res = await postComment({
-        content: search,
-        image: "",
-      });
-      
-      setSearch("");
-    } catch (error) {
-      throw error;
-    }
-  };
+	const handleSubmit = async () => {
+		try {
+			const res = await createPost({
+				content: search,
+				image: "",
+			});
 
-  return (
-    <div className="flex-1 flex-grow text-white overflow-y-auto pb-20 scroll-bg-darkgrey pe-2">
-      <div className="bg-grey flex justify-around gap-5 p-7 rounded-2xl mb-5  message-sender">
-        <img
-          src="https://picsum.photos/200/300"
-          alt="profile"
-          className="w-12 h-12 rounded-full"
-        />
-        <div className="flex flex-1 gap-5 ">
-          <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                // Trigger form submit when Enter key is pressed
-                handleSubmit();
-              }
-            }}
-            className="bg-darkgrey text-white placeholder:text-slate-400 w-full outline-none text-sm rounded-xl p-2 h-12 px-4 flex-1"
-            placeholder="What's happening?"
-          />
-          <button
-            onClick={handleSubmit}
-            className="bg-blue-500 hover:bg-blue-600 transition duration-300 ease-in-out text-white w-12 h-12 rounded-xl flex items-center justify-center"
-          >
-            <i className="fas fa-paper-plane"></i>
-          </button>
-        </div>
-      </div>
-      {Array.from({ length: 10 }).map((_, i) => (
-        <Post key={i} />
-      ))}
-    </div>
-  );
+			setSearch("");
+		} catch (error) {
+			throw error;
+		}
+	};
+
+	return (
+		<div className="flex-1 flex-grow text-white overflow-y-auto pb-20 scroll-bg-darkgrey pe-2">
+			<div className="bg-grey flex justify-around gap-5 p-7 rounded-2xl mb-5  message-sender">
+				<img
+					src="https://picsum.photos/200/300"
+					alt="profile"
+					className="w-12 h-12 rounded-full"
+				/>
+				<div className="flex flex-1 gap-5 ">
+					<input
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								// Trigger form submit when Enter key is pressed
+								handleSubmit();
+							}
+						}}
+						className="bg-darkgrey text-white placeholder:text-slate-400 w-full outline-none text-sm rounded-xl p-2 h-12 px-4 flex-1"
+						placeholder="What's happening?"
+					/>
+					<button
+						onClick={handleSubmit}
+						className="bg-blue-500 hover:bg-blue-600 transition duration-300 ease-in-out text-white w-12 h-12 rounded-xl flex items-center justify-center"
+					>
+						<i className="fas fa-paper-plane"></i>
+					</button>
+				</div>
+			</div>
+			{Array.from({ length: 10 }).map((_, i) => (
+				<Post key={i} />
+			))}
+		</div>
+	);
 };
--- src/components/app/Post/Post.js
+++ src/components/app/Post/Post.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { postComment } from "../../../shared/services/post.service";
+
 
 const Post = () => {
   const [show, setShow] = React.useState(false);
