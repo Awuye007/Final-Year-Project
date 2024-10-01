f4c1234df2d3e590576026e4dd63464d39ea1523
Jason Addy <jasonaddy51@gmail.com>
2024-10-01 11:37:50 +0000

    Refactor Counsellors component to fetch and display counsellor data from Firebase

--- src/pages/Main/Counsellors/Counsellors.js
+++ src/pages/Main/Counsellors/Counsellors.js
@@ -1,27 +1,54 @@
+import { collection, getDocs } from "firebase/firestore";
 import React from "react";
+import { NoContent } from "../../../components/app/NoContent/NoContent";
+import { db } from "../../../shared/core/firebase";
 
 const Counsellors = () => {
+  const [counsellors, setCounsellors] = React.useState([]);
+
+  React.useEffect(() => {
+    getCounsellors();
+  }, []);
+
+  const getCounsellors = async () => {
+    try {
+      const resp = await getDocs(collection(db, "counsellors"));
+      if (!resp) return;
+      const data = resp.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setCounsellors(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-full flex flex-wrap gap-5 overflow-y-auto pb-24">
-      {Array(10)
-        .fill(0)
-        .map((_, index) => (
-          <div
-            className="h-40 md:h-60 bg-grey rounded-3xl p-5 w-full md:w-[30%] flex flex-col items-center gap-3 justify-center"
-            key={index}
+      {counsellors.map((counsellor, index) => (
+        <div
+          className="h-40 md:h-60 bg-grey rounded-3xl p-5 w-full md:w-[30%] flex flex-col items-center gap-3 justify-center"
+          key={index}
+        >
+          <h5 className="text-white font-medium text-2xl mb-2">
+            {counsellor?.name}
+          </h5>
+          <p className="text-slate-300 mb-2">Fluent in Ga & Twi</p>
+          <a
+            className="text-muted text-sm"
+            href={`https://wa.me/+233${counsellor?.phone.slice(1)}`}
+            target="_blank"
           >
-            <h5 className="text-white font-medium text-2xl mb-2">
-              Jeffery Awuye
-            </h5>
-            <p className="text-slate-300 mb-2">Fluent in Ga & Twi</p>
-            <a
-              className="text-muted text-sm"
-              href="https://wa.me/+233507764226"
-            >
-              0507764226
-            </a>
-          </div>
-        ))}
+            {counsellor?.phone}
+          </a>
+        </div>
+      ))}
+      {counsellors.length === 0 && (
+        <div className="w-full flex justify-center">
+          <NoContent
+            title="No Counsellors Found"
+            description="No counsellors found in the database"
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Main/Events/Events.js
+++ src/pages/Main/Events/Events.js
@@ -1,33 +1,73 @@
+import { collection, getDocs } from "firebase/firestore";
 import React from "react";
+import { NoContent } from "../../../components/app/NoContent/NoContent";
 import {
   Accordion,
   AccordionItem,
 } from "../../../components/ui/Accordion/Accordion";
+import { db } from "../../../shared/core/firebase";
 
 const Events = () => {
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    getEvents();
+  }, []);
+
+  const getEvents = async () => {
+    try {
+      const resp = await getDocs(collection(db, "events"));
+      if (!resp) return;
+      const data = resp.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setEvents(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-full overflow-y-auto">
       <Accordion className="flex flex-wrap gap-5">
-        {Array(10)
-          .fill(0)
-          .map((_, index) => (
-            <AccordionItem
-              value={index + 1}
-              trigger={`Event ${index + 1}`}
-              className="md:max-w-[30%] mb-5"
-              key={index}
-            >
-              <p className="text-slate-400 text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                maximus nibh at dolor vehicula faucibus. Praesent mattis rutrum
-                auctor. Cras nec sodales risus, ac porttitor metus. Pellentesque
-                interdum nisi neque, id tempus ex aliquam sit amet. Integer
-                laoreet, dolor in tempus blandit, nibh urna scelerisque nisl,
-                sed tristique enim ipsum et nibh.
-              </p>
-            </AccordionItem>
-          ))}
+        {events.map((event, index) => (
+          <AccordionItem
+            value={index + 1}
+            trigger={event?.name}
+            className="md:max-w-[30%] mb-5"
+            key={index}
+          >
+            <p className="text-slate-400 text-sm">
+              <table className="table-auto w-full">
+                <tbody>
+                  <tr>
+                    <td className="font-bold pr-10">Name</td>
+                    <td>{event?.name}</td>
+                  </tr>
+                  <tr>
+                    <td className="font-bold pr-10">Date</td>
+                    <td>{event?.date}</td>
+                  </tr>
+                  <tr>
+                    <td className="font-bold pr-10">Location</td>
+                    <td>{event?.location}</td>
+                  </tr>
+                  <tr>
+                    <td className="font-bold align-top pr-10">Description</td>
+                    <td>{event?.description}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </p>
+          </AccordionItem>
+        ))}
       </Accordion>
+      {events?.length === 0 && (
+        <div className="w-full flex justify-center h-screen">
+          <NoContent
+            title="No Events Found"
+            description="No events found in the database"
+          />
+        </div>
+      )}
     </div>
   );
 };
